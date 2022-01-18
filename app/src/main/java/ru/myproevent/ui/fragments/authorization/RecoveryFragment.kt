2b2be56d1e2ee8c29c0646f60fa7c2c058a7c218
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2022-01-18 14:58:44 +0300

    Обновил экран восстановления пароля

--- app/src/main/java/ru/myproevent/ui/fragments/authorization/RecoveryFragment.kt
+++ app/src/main/java/ru/myproevent/ui/fragments/authorization/RecoveryFragment.kt
@@ -2,9 +2,12 @@ package ru.myproevent.ui.fragments.authorization
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import moxy.ktx.moxyPresenter
 import ru.myproevent.ProEventApp
 import ru.myproevent.databinding.FragmentRecoveryBinding
+import ru.myproevent.domain.utils.pxValue
 import ru.myproevent.ui.fragments.BaseMvpFragment
 import ru.myproevent.ui.presenters.authorization.recovery.RecoveryPresenter
 import ru.myproevent.ui.presenters.authorization.recovery.RecoveryView
@@ -12,6 +15,28 @@ import ru.myproevent.ui.presenters.main.RouterProvider
 
 class RecoveryFragment : BaseMvpFragment<FragmentRecoveryBinding>(FragmentRecoveryBinding::inflate),
     RecoveryView {
+
+    private fun setLayoutParams() = with(binding) {
+        body.post {
+            val availableHeight = root.height
+
+            if (ohNowIRemember.lineCount > 1 || authorize.lineCount > 1) {
+                bottomOptionsContainer.orientation = LinearLayout.VERTICAL
+                bottomOptionsHorizontalSeparator.visibility = View.GONE
+            }
+
+            bodySpace.layoutParams = bodySpace.layoutParams.apply { height = availableHeight }
+            body.post {
+                val difference = body.height - availableHeight
+                if (difference > 0) {
+                    logo.isVisible = false
+                }
+                if (difference > pxValue(80f + 48f)) {
+                    formTitle.isVisible = false
+                }
+            }
+        }
+    }
 
     override val presenter by moxyPresenter {
         RecoveryPresenter((parentFragment as RouterProvider).router).apply {
@@ -25,6 +50,7 @@ class RecoveryFragment : BaseMvpFragment<FragmentRecoveryBinding>(FragmentRecove
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.authorizeHitArea.setOnClickListener { presenter.authorize() }
+        setLayoutParams()
+        binding.bottomOptionsContainer.setOnClickListener { presenter.authorize() }
     }
 }
